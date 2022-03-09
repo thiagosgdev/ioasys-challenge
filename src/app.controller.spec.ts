91a8e4a860fbaa35e0b7f20318dae47a0c6cc057
Thiago Gonçalves <thiagosgdev@gmail.com>
2022-03-08 21:14:24 -0300

    test: refactoring health controller tests and env test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,18 @@
+import {
+  HealthCheckService,
+  HttpHealthIndicator,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
+import { HealthCheckExecutor } from '@nestjs/terminus/dist/health-check/health-check-executor.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+const mockHealthCheckService = {
+  create: jest.fn((dto) => {
+    return { status: 200 };
+  }),
+};
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,8 +20,17 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+      providers: [
+        AppService,
+        HealthCheckService,
+        HealthCheckExecutor,
+        HttpHealthIndicator,
+        TypeOrmHealthIndicator,
+      ],
+    })
+      .overrideProvider(HealthCheckService)
+      .useValue(mockHealthCheckService)
+      .compile();
 
     appController = app.get<AppController>(AppController);
   });
---
+++ src/utils/health/health.controller.spec.ts
@@ -0,0 +1,30 @@
+import {
+  HealthCheckService,
+  HttpHealthIndicator,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
+import { HealthCheckExecutor } from '@nestjs/terminus/dist/health-check/health-check-executor.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { HealthController } from './health.controller';
+
+describe('HealthController', () => {
+  let controller: HealthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HealthController],
+      providers: [
+        HealthCheckService,
+        HealthCheckExecutor,
+        HttpHealthIndicator,
+        TypeOrmHealthIndicator,
+      ],
+    }).compile();
+
+    controller = module.get<HealthController>(HealthController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
