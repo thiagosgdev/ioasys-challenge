c274bd3a75338261c2f5c8631e0342b832cfdaf2
Thiago Gonçalves <thiagosgdev@gmail.com>
2022-03-24 09:28:49 -0300

    chore: disabling helmet to test swagger docs on cloud

--- src/main.ts
+++ src/main.ts
@@ -19,16 +19,6 @@ async function bootstrap() {
     .build();
 
   app.useGlobalPipes(new ValidationPipe());
-  app.use(helmet(), {
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: [`'self'`],
-        styleSrc: [`'self'`, `'unsafe-inline'`],
-        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
-        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
-      },
-    },
-  });
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
