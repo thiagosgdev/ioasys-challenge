1f3bf07dc19b8fd9cc257a7e02e665f0822e79b4
Thiago Gonçalves <thiagosgdev@gmail.com>
2022-03-31 18:35:36 -0300

    fix: changed http method

--- src/modules/users/context/logOut/logout.controller.ts
+++ src/modules/users/context/logOut/logout.controller.ts
@@ -1,28 +1,31 @@
 import {
   Controller,
-  Get,
   Headers,
   HttpCode,
   HttpException,
   HttpStatus,
+  Post,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
-import { LogoutService } from './logout.service';
+import { LogoutService } from 'src/modules/users/context/logOut/logout.service';
 
 @ApiTags('users')
 @Controller('/users')
 export class LogoutController {
   constructor(private logoutService: LogoutService) {}
 
-  @Get('/logout')
+  @Post('/logout')
   @HttpCode(HttpStatus.NO_CONTENT)
   public async handle(@Headers('Authorization') auth: string) {
     try {
       const token = auth.split(' ')[1];
       await this.logoutService.logout(token);
     } catch (error) {
-      throw new HttpException(error.message, error.response);
+      throw new HttpException(
+        error.response.message,
+        error.response.statusCode,
+      );
     }
   }
 }
