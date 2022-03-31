3affc2bd75d2ea3a791679ecb8c8b76eadb0b011
Thiago Gonçalves <thiagosgdev@gmail.com>
2022-03-31 18:02:55 -0300

    fix: new cors test

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,11 @@ import { AppModule } from 'src/app.module';
 async function bootstrap() {
   const logger = WinstonModule.createLogger(winstonConfig);
   const app = await NestFactory.create(AppModule, { logger });
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.use(
     helmet({
       contentSecurityPolicy: false,
