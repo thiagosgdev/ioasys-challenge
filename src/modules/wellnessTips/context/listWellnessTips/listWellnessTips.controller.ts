e498f0d05786644b9b0a23fac46b949d9aa010f3
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2022-04-20 22:01:59 -0300

    refactor: changed wellness route to public

--- src/modules/wellnessTips/context/listWellnessTips/listWellnessTips.controller.ts
+++ src/modules/wellnessTips/context/listWellnessTips/listWellnessTips.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, HttpException } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
-import { WellnessTipsResponseDTO } from 'src/shared/dtos/wellness/wellnessResponse';
 
+import { Public } from 'src/shared/decorators/public.decorator';
+import { WellnessTipsResponseDTO } from 'src/shared/dtos/wellness/wellnessResponse';
 import { ApiCommomDecorators } from '../../../../shared/decorators/globalDoc.decorator';
 import { ListWellnessTipsService } from './listWellnessTips.service';
 
@@ -10,6 +11,7 @@ import { ListWellnessTipsService } from './listWellnessTips.service';
 export class ListWellnessTipsController {
   constructor(private listWellnessTipsService: ListWellnessTipsService) {}
 
+  @Public()
   @Get()
   @ApiOkResponse({
     description: 'A list wellness tips will be returned',
